4eee2d2968c9505954465cc3c36e34724e4a0a36
Daniel Sun <hjdaniel.sun@gmail.com>
2018-04-21 01:22:44 +1000

    fixed an error in run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 
 @echo off
 
-for /l %%i in (1,1,4) do start cmd /k "for /l %%h in (2000,2000,10000) do (python run.py %%h False Create && python run.py %%h True Read)"
+for /l %%i in (1,1,4) do start cmd /k "for /l %%h in (2000,2000,10000) do (python run.py %%h False create && python run.py %%h True read)"
 
 
